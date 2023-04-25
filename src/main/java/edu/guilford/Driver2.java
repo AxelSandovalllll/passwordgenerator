f79d12cba42949bf70520ee18f6e62cd8a27c478
Axel <axelsp050@gmail.com>
2023-04-24 21:16:03 -0400

    fixed the code to have 2 users and 2 randomly generated  passwords, along with 2 encryptions and decryptions.

--- src/main/java/edu/guilford/Driver2.java
+++ src/main/java/edu/guilford/Driver2.java
@@ -27,7 +27,7 @@ public class Driver2 {
 
         axel.generatePassword(firstName, lastName, color, number);
         // print out the password
-        System.out.println("Your password is: " + axel.getPassword());
+        System.out.println(axel.getFirstName() + "'s randomly generated password is: " + axel.getPassword());
 
         AES aes = new AES();
 
@@ -36,15 +36,46 @@ public class Driver2 {
         aes.setKey("ThisisASecretKey");
         String strToEncrypt = axel.getPassword();
         String encryptedPassword = aes.encrypt(strToEncrypt, "ThisisASecretKey");
-        System.out.println("Your encrypted password is: " + encryptedPassword);
+        System.out.println(axel.getFirstName() + "'s encrypted password is: " + encryptedPassword);
 
         // decrypt the password using the AES class
         String strToDecrypt = encryptedPassword;
         String decryptedPassword = aes.decrypt(strToDecrypt, "ThisisASecretKey");
-        System.out.println("Your decrypted password is: " + decryptedPassword +
+        System.out.println(axel.getFirstName() + "'s decrypted password is: " + decryptedPassword +
                 "\n");
 
-        // second user
+        User2 bob = new User2();
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String firstName2 = scan2.nextLine();
+        bob.setFirstName(firstName2);
+
+        System.out.println("What is your last name?");
+        String lastName2 = scan2.nextLine();
+        bob.setLastName(lastName2);
+
+        System.out.println("What is your favorite color?");
+        String color2 = scan2.nextLine();
+        bob.setColor(color2);
+
+        System.out.println("What is your favorite number?");
+        int number2 = scan2.nextInt();
+        bob.setNumber(number2);
+
+        bob.generatePassword(firstName2, lastName2, color2, number2);
+        System.out.println(bob.getFirstName() + "'s randomly generated password is: " + bob.getPassword());
+
+        aes.setKey("ThisisASecretKey");
+        String strToEncrypt2 = bob.getPassword();
+        String encryptedPassword2 = aes.encrypt(strToEncrypt2, "ThisisASecretKey");
+        System.out.println(bob.getFirstName() + "'s encrypted password is: " + encryptedPassword2);
+
+        // decrypt the password using the AES class
+        String strToDecrypt2 = encryptedPassword2;
+        String decryptedPassword2 = aes.decrypt(strToDecrypt2, "ThisisASecretKey");
+        System.out.println(bob.getFirstName() + "'s decrypted password is: " + decryptedPassword2 +
+                "\n");
+
     }
 
 }
